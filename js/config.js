fb317df1e7337aef5a7814665effdb2d6be211cf
Elto Borges <eltobrgz.code@gmail.com>
2025-07-06 23:16:56 -0300

    atualizaçoes de conexao

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Configuração centralizada da API
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://styleback.onrender.com';
 const API_URL = `${API_BASE_URL}/api`;
 
 // Exportar as configurações para uso em outros arquivos
